e6c331ae4b6eae7f44311a3c69bcc8df446b9d77
Anastasiia Kuznetsova <toph28555@gmail.com>
2022-05-10 10:26:52 +0300

    add viewpager with forecasts

--- app/src/main/java/com/github/skytoph/simpleweather/presentation/fragment/ForecastFragment.kt
+++ app/src/main/java/com/github/skytoph/simpleweather/presentation/fragment/ForecastFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.skytoph.simpleweather.R
+import com.github.skytoph.simpleweather.databinding.FragmentForecastBinding
 
 class ForecastFragment : Fragment() {
+    private lateinit var binding: FragmentForecastBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +18,10 @@ class ForecastFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forecast, container, false)
+    ): View {
+        binding = FragmentForecastBinding.inflate(layoutInflater, container, false)
+        return binding.root
+//        return inflater.inflate(R.layout.fragment_forecast, container, false)
     }
 
 }
