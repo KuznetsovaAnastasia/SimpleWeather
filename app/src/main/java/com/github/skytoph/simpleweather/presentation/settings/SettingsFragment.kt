b16900366123eab44e3d32e6239fb18be911f5b0
Anastasiia Kuznetsova <toph28555@gmail.com>
2022-10-12 13:35:45 +0300

    add languages support in settings

--- app/src/main/java/com/github/skytoph/simpleweather/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/github/skytoph/simpleweather/presentation/settings/SettingsFragment.kt
@@ -1,12 +1,36 @@
 package com.github.skytoph.simpleweather.presentation.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.github.skytoph.simpleweather.R
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
+
+    override fun onResume() {
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .registerOnSharedPreferenceChangeListener(this)
+        super.onResume()
+    }
+
+    override fun onPause() {
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .unregisterOnSharedPreferenceChangeListener(this)
+        super.onPause()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) =
+        if (key == resources.getString(R.string.key_language)) {
+            val language = sharedPreferences.getString(key, resources.getString(R.string.language_eng))
+            val locale = LocaleListCompat.forLanguageTags(language)
+            AppCompatDelegate.setApplicationLocales(locale)
+        } else Unit
 }
