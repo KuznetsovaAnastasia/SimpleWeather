b4529a61a790591cdb649d46d7e5a2e384b2f049
Anastasiia Kuznetsova <toph28555@gmail.com>
2022-12-08 17:52:16 +0200

    set default language preference value

--- app/src/main/java/com/github/skytoph/simpleweather/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/github/skytoph/simpleweather/presentation/settings/SettingsFragment.kt
@@ -1,12 +1,15 @@
 package com.github.skytoph.simpleweather.presentation.settings
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.viewModels
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.github.skytoph.simpleweather.R
 import com.github.skytoph.simpleweather.presentation.favorites.ConfirmationDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -17,16 +20,26 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
         setupClearHistoryPreference()
+        setupLanguagePreference()
     }
 
     private fun setupClearHistoryPreference() {
         preferenceScreen.findPreference<Preference>(getString(R.string.key_clear_search_history))
             ?.setOnPreferenceClickListener {
                 ConfirmationDialogFragment
-                    .newInstance({ viewModel.clearSearchHistory() }, R.string.clear_search_history_confirmation)
+                    .newInstance({ viewModel.clearSearchHistory() },
+                        R.string.clear_search_history_confirmation)
                     .show(parentFragmentManager, DIALOG_TAG)
                 true
             }
+    }
+
+    private fun setupLanguagePreference() {
+        val preference =
+            preferenceScreen.findPreference<ListPreference>(getString(R.string.key_language))
+        val language = Locale.getDefault().language
+        val indexOfValue = preference?.findIndexOfValue(language).takeIf { it != -1 } ?: 0
+        preference?.setValueIndex(indexOfValue)
     }
 
     private companion object {
