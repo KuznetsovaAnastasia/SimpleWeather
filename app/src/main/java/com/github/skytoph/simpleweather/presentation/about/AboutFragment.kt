c24f5291e1b6b7b095fd05e070361dc2fa44d53b
toph <toph28555@gmail.com>
2023-06-20 17:52:11 +0300

    add content to about page

--- app/src/main/java/com/github/skytoph/simpleweather/presentation/about/AboutFragment.kt
+++ app/src/main/java/com/github/skytoph/simpleweather/presentation/about/AboutFragment.kt
@@ -1,13 +1,27 @@
 package com.github.skytoph.simpleweather.presentation.about
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import com.github.skytoph.simpleweather.core.presentation.BaseBindingFragment
-import com.github.skytoph.simpleweather.databinding.FragmentSearchBinding
+import androidx.fragment.app.viewModels
+import com.github.skytoph.simpleweather.R
+import com.github.skytoph.simpleweather.core.presentation.BaseFragment
+import com.github.skytoph.simpleweather.databinding.FragmentAboutBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class AboutFragment : BaseBindingFragment<FragmentSearchBinding>() {
+@AndroidEntryPoint
+class AboutFragment : BaseFragment<AboutViewModel, FragmentAboutBinding>() {
 
-    override val bindingInflation:
-                (inflater: LayoutInflater, container: ViewGroup?, attachToParent: Boolean) -> FragmentSearchBinding
-        get() = FragmentSearchBinding::inflate
+    override val viewModel by viewModels<AboutViewModel>()
+
+    override val bindingInflation: (LayoutInflater, ViewGroup?, Boolean) -> FragmentAboutBinding =
+        FragmentAboutBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.appbarSettings.toolbarTitle.text = resources.getString(R.string.about_app)
+        binding.appbarSettings.toolbarSettings.setNavigationIcon(R.drawable.ic_back)
+        binding.appbarSettings.toolbarSettings.setNavigationOnClickListener { viewModel.goBack() }
+    }
 }
